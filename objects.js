5bf55a40c81a2b4d1529c0759058168e1229524c
Garrett Halstein <garrett.halstein@emory.edu>
2019-01-19 05:43:13 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,5 @@
 var playlist = {artistName: 'song'};
+<<<<<<< HEAD
 function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle;
   return playlist;
@@ -7,3 +8,5 @@ function removeFromPlaylist(playlist, artistName){
   delete playlist[artistName];
   return playlist;
 }
+=======
+>>>>>>> 08771e2879165c0a51c925e7b1a82107becc36fa
